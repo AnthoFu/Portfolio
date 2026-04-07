5399aa1e544f7acf4a72f98a03a133b70298031e
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-04-07 06:51:53 -0400

    [V0.5.0] feat: Implemetancion inicial de la estructura de Projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Project bla bla bla',
+      description: 'Project bla bla bla, para el bla bla bla y el bla bla bla, bla bla bla y bla bla bla.',
+      images: [
+        'images/banner-pc.png',
+        'images/banner-cel.png',
+        'images/Anthofu_photo-1.jpeg'
+      ],
+      selectedImageIndex: 0
+    }
+  ];
 
+  selectImage(projectIndex: number, imageIndex: number) {
+    this.projects[projectIndex].selectedImageIndex = imageIndex;
+  }
 }
