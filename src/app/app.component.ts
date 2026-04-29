2186b4fa6a6a000a674c6692774c0f0be552179b
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-04-29 19:33:40 -0400

    [V0.10.1] feat: Agregado efecto de ocultamiento del header en PC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
@@ -12,6 +12,32 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
   isMenuOpen = false;
+  isScrolled = false;
+  isVisible = true;
+  private lastScrollTop = 0;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      const currentScroll = window.scrollY;
+      
+      // Determine if background should be solid
+      this.isScrolled = currentScroll > 50;
+
+      // Determine visibility (Smart Header)
+      if (currentScroll > this.lastScrollTop && currentScroll > 100) {
+        // Scrolling down - hide
+        this.isVisible = false;
+      } else {
+        // Scrolling up - show
+        this.isVisible = true;
+      }
+      
+      this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    }
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
